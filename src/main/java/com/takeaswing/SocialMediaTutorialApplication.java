6b73326e6fad66c4f41fdba235f79f0225074f94
deep <deep@deep-HP-Laptop-15-bs0xx>
2026-01-19 19:44:19 +0530

    initial commit

---
+++ src/main/java/com/takeaswing/SocialMediaTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.takeaswing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/takeaswing/SocialMediaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.takeaswing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
